8bed2ab463103d29f1309bea44d83ffb33562b5f
rka <enrev91@gmail.com>
2018-06-27 03:29:41 +0200

    Practical 2

--- practical2.py
+++ practical2.py
@@ -1,5 +1,6 @@
 import torch.tensor as Tensor
 import torch as torch
+import dlc_practical_prologue as prologue
 
 
 # 1 - Nearest Neighbor
@@ -66,23 +67,50 @@ def generate_dataset(size, dim, num_classes):
     return T, target, x, x_target
 
 
-def test_nearest_classification(T, target, x, x_target, embedding_dim):
-    non_pca_error = compute_nb_errors(T, target, x, x_target)
-    train_mean, train_basis = pca(T)
-    train_embedding, train_recon = embed_target(train_mean, train_basis, T, embedding_dim)
-    test_embedding, test_recon = embed_target(train_mean, train_basis, x, embedding_dim)
-    with_pca_error = compute_nb_errors(train_embedding, target, test_embedding, x_target)
-    return non_pca_error, with_pca_error
+def test_nearest_classification(T, target, x, x_target, embedding_dim = None):
+    if embedding_dim is not None:
+        train_mean, train_basis = pca(T)
+        train_embedding, train_recon = embed_target(train_mean, train_basis, T, embedding_dim)
+        test_embedding, test_recon = embed_target(train_mean, train_basis, x, embedding_dim)
+        with_pca_error = compute_nb_errors(train_embedding, target, test_embedding, x_target)
+        return with_pca_error
+    else:
+        non_pca_error = compute_nb_errors(T, target, x, x_target)
+        return non_pca_error
 
 
-T, target, x, x_target = generate_dataset(1000, 100, 5)
-base_error, error_100 = test_nearest_classification(T, target, x, x_target, 100)
-_, error_50 = test_nearest_classification(T, target, x, x_target, 50)
-_, error_10 = test_nearest_classification(T, target, x, x_target, 10)
-_, error_3 = test_nearest_classification(T, target, x, x_target, 3)
-print("Test set size: %i samples" % 1000)
-print("Number of Errors without PCA: \t \t %i" % base_error)
-print("Number of Errors with 100d PCA: \t %i" % error_100)
-print("Number of Errors with 50d PCA: \t \t %i" % error_50)
-print("Number of Errors with 10d PCA: \t \t %i" % error_10)
-print("Number of Errors with 3d PCA: \t \t %i" % error_3)
+def get_mnist_datasets():
+    mnist_train, mnist_train_target, mnist_test, mnist_test_target = prologue.load_data(cifar=False)
+    return mnist_train, mnist_train_target, mnist_test, mnist_test_target
+
+
+def get_cifar_datasets():
+    cifar_train, cifar_train_target, cifar_test, cifar_test_target = prologue.load_data(cifar=True)
+    return cifar_train, cifar_train_target, cifar_test, cifar_test_target
+
+
+def test_data(train, train_target, x, x_target):
+    base_error = test_nearest_classification(train, train_target, x, x_target)
+    error_100 = test_nearest_classification(train, train_target, x, x_target, 100)
+    error_50 = test_nearest_classification(train, train_target, x, x_target, 50)
+    error_10 = test_nearest_classification(train, train_target, x, x_target, 10)
+    error_3 = test_nearest_classification(train, train_target, x, x_target, 3)
+    print("Test set size: %i samples" % 1000)
+    print("Number of Errors without PCA: \t \t %i" % base_error)
+    print("Number of Errors with 100d PCA: \t %i" % error_100)
+    print("Number of Errors with 50d PCA: \t \t %i" % error_50)
+    print("Number of Errors with 10d PCA: \t \t %i" % error_10)
+    print("Number of Errors with 3d PCA: \t \t %i" % error_3)
+
+
+if __name__ == '__main__':
+    #print("Random Data...\n")
+    #train, train_target, x, x_target = generate_dataset(1000, 100, 5)
+    #test_data(train, train, x, x_target)
+    print("MNIST...\n")
+    mnist_train, mnist_train_target, mnist_test, mnist_test_target = get_mnist_datasets()
+    test_data(mnist_train, mnist_train_target, mnist_test, mnist_test_target)
+    print("CIFAR...\n")
+    cifar_train, cifar_train_target, cifar_test, cifar_test_target = get_cifar_datasets()
+    test_data(cifar_train, cifar_train_target, cifar_test, cifar_test_target)
+
